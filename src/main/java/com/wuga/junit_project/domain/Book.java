33e716fcf233de533b331a88dac881cd9350b290
Wugawuga <dnr3655@naver.com>
2022-07-19 16:42:31 +0900

    create Book Entity

--- src/main/java/com/wuga/junit_project/domain/Book.java
+++ src/main/java/com/wuga/junit_project/domain/Book.java
@@ -1,5 +1,34 @@
 package com.wuga.junit_project.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Book {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
