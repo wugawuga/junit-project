360cb0d6302986220991002c65dad9fdcb33b0ed
Wugawuga <dnr3655@naver.com>
2022-07-27 23:37:17 +0900

    Add Book select test, tableInit.sql

--- src/test/java/com/wuga/junit_project/domain/BookRepositoryTest.java
+++ src/test/java/com/wuga/junit_project/domain/BookRepositoryTest.java
@@ -4,15 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest // db 관련 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
-    
+
     @Autowired
     private BookRepository bookRepository;
+
+    @BeforeEach
+    public void 책_데이터_준비() {
+        String title = "junit5";
+        String author = "wuga";
+
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    }
 
     @Test
     public void 책등록_테스트() {
@@ -21,9 +35,9 @@ public class BookRepositoryTest {
         String author = "wuga";
 
         Book book = Book.builder()
-            .title(title)
-            .author(author)
-            .build();
+                .title(title)
+                .author(author)
+                .build();
 
         // 테스트 실행
         Book bookEntity = bookRepository.save(book);
@@ -39,12 +53,6 @@ public class BookRepositoryTest {
         String title = "junit5";
         String author = "wuga";
 
-        Book book = Book.builder()
-            .title(title)
-            .author(author)
-            .build();
-        bookRepository.save(book);
-
         // 테스트 실행
         List<Book> bookEntity = bookRepository.findAll();
 
@@ -53,4 +61,18 @@ public class BookRepositoryTest {
         assertEquals(author, bookEntity.get(0).getAuthor());
     }
 
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책한권보기_테스트() throws Exception {
+        // 데이터 준비
+        String title = "junit5";
+        String author = "wuga";
+
+        // 테스트 실행
+        Book bookEntity = bookRepository.findById(1L).get();
+
+        // 검증
+        assertEquals(title, bookEntity.getTitle());
+        assertEquals(author, bookEntity.getAuthor());
+    }
 }
