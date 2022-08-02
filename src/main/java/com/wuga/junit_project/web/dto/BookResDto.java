7eb9dc98fff30fbfae045ae7abd9705469302f08
Wugawuga <dnr3655@naver.com>
2022-08-02 23:55:18 +0900

    Add BookService insert

--- src/main/java/com/wuga/junit_project/web/dto/BookResDto.java
+++ src/main/java/com/wuga/junit_project/web/dto/BookResDto.java
@@ -1,5 +1,22 @@
 package com.wuga.junit_project.web.dto;
 
+import com.wuga.junit_project.domain.Book;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BookResDto {
     
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookResDto toDto(Book bookEntity) {
+        this.id = bookEntity.getId();
+        this.title = bookEntity.getTitle();
+        this.author = bookEntity.getAuthor();
+        return this;
+    }
 }
--- src/test/java/com/wuga/junit_project/service/BookServiceTest.java
+++ src/test/java/com/wuga/junit_project/service/BookServiceTest.java
@@ -1,5 +1,26 @@
 package com.wuga.junit_project.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wuga.junit_project.domain.Book;
+import com.wuga.junit_project.domain.BookRepository;
+import com.wuga.junit_project.web.dto.BookResDto;
+import com.wuga.junit_project.web.dto.BookSaveReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class BookServiceTest {
     
+    private final BookRepository bookRepository;
+
+    // 1. 책등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookResDto 책등록하기_테스트(BookSaveReqDto bookSaveReqDto) {
+
+        Book bookEntity = bookRepository.save(bookSaveReqDto.toEntity());
+        return new BookResDto().toDto(bookEntity);
+    }
 }
