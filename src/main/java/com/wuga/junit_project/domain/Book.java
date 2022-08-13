cf254123c6830072b99f0e65e0f3f5b39047d22e
Wugawuga <dnr3655@naver.com>
2022-08-13 22:39:44 +0900

    Add update method to Book

--- src/main/java/com/wuga/junit_project/domain/Book.java
+++ src/main/java/com/wuga/junit_project/domain/Book.java
@@ -31,4 +31,10 @@ public class Book {
         this.title = title;
         this.author = author;
     }
+
+    public void update(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
 }
